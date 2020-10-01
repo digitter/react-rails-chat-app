99b63493b6db08a6e1b5ede66a82b5917275c697
kazuki <matchingserver@gmail.com>
2020-10-02 00:46:37 +0900

    setup broadcasting for universal and non-universal channel

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+  def create
+    message = Message.new(message_params)
+    conversation = Conversation.find(message_params[:conversation_id])
+
+    if message.save
+      serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        MessageSerializer.new(message)
+      ).serializable_hash
+
+      # insted of string, this method takes an object from model as first argument.
+      # stream_for and broadcast_to is useful for transmitting data along non-universal channels, such as for members  of a particular conversation or specific users.
+      MessagesChannel.broadcast_to conversation, serialized_data
+      head :ok
+    end
+  end
+end
